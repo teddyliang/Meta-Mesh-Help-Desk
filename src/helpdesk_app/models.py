0faa15d7d3e3182f178729fbdc5cf2cc63d1cc6d
robloxosf <ayoubman@yahoo.com>
2023-03-20 22:39:10 -0400

    [M2] Create Django resources for resources & keywords (#27)

--- src/helpdesk_app/models.py
+++ src/helpdesk_app/models.py
@@ -16,6 +16,29 @@ class Profile(models.Model):
     )
 
 
+# Model to represent resources
+class AnswerResource(models.Model):
+    # Short title to describe the resource (required)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    # URL associated with the resource (required, must be unique)
+    url = models.URLField(max_length=1000, unique=True, blank=False, null=False)
+    # Blurb to describe the resource (required)
+    blurb = models.CharField(max_length=2500, blank=True, default='')
+    # Last updated
+    updated = models.DateTimeField(auto_now=True)
+
+
+# Model to represen a keyword (resources can have many keywords)
+class Keyword(models.Model):
+    # The resource this keyword belongs to
+    # If the resource gets deleted, all keywords will be deleted as well (CASCADE)
+    resource = models.ForeignKey(AnswerResource, on_delete=models.CASCADE, blank=True, null=True)
+    # Short word or phrase to describe the keyword
+    name = models.CharField(max_length=100, blank=False, null=False)
+    # Last updated
+    updated = models.DateTimeField(auto_now=True)
+
+
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
