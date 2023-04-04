c8a868ea8e173e8627268b42c43bd8c52050ea1e
ayoubmansar <ayoubman@yahoo.com>
2023-04-04 02:28:47 -0400

    [M3] Add ability to view categories (#12)

--- src/helpdesk_app/filters.py
+++ src/helpdesk_app/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 from django_filters import DateFilter, OrderingFilter, CharFilter
-from .models import User, AnswerResource
+from .models import Category, User, AnswerResource
 
 
 class UserFilter(django_filters.FilterSet):
@@ -63,3 +63,9 @@ class ResourceFilter(django_filters.FilterSet):
     def __init__(self, *args, **kwargs):
         super(ResourceFilter, self).__init__(*args, **kwargs)
         self.filters['updated'].label = "Last updated date is on or after:"
+
+
+class CategoryFilter(django_filters.FilterSet):
+    class Meta:
+        model = Category
+        fields = ['category_name']
