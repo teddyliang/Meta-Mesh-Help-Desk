6402f31a20e76bc625e3d420869a1726e53aa4bd
Nicholas Clark <nicholas@califoriaclarks.com>
2023-04-24 23:32:54 -0400

    FAQ done

--- src/helpdesk_app/urls.py
+++ src/helpdesk_app/urls.py
@@ -36,4 +36,5 @@ urlpatterns = [
     path('update_category/<int:id>', views.update_category, name="update_category"),
     path('delete_category/<int:id>', views.delete_category, name="delete_category"),
     path('categories/', views.view_categories, name="categories"),
+    path('get_faq', views.get_faq, name="get_faq"),
 ]
